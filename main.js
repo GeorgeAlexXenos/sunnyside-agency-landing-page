1b95b8d0ceaa4444b828e18159dfc7ff8ae1fe6d
georgealex <georgealex1982@gmail.com>
2021-12-14 16:52:46 +0200

    last commit

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let navbar = document.querySelector(".navbar");
 hamburgerIcon.addEventListener("click", function () {
   navbar.classList.toggle("visibility");
 });
+
+let logo = document.querySelectorAll(".logo");
+console.log(logo);
+
+for (let i = 0; i < logo.length; i++) {
+  logo[i].addEventListener("click", function () {
+    location.href = "index.html";
+  });
+}
