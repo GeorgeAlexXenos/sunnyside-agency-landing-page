1fc21962d64a90ba2fbe38a367b7ab617e198c45
georgealex <georgealex1982@gmail.com>
2021-12-19 14:48:43 +0200

    some testing

--- main.js
+++ main.js
@@ -5,10 +5,25 @@ hamburgerIcon.addEventListener("click", function () {
 });
 
 let logo = document.querySelectorAll(".logo");
-console.log(logo);
+// console.log(logo);
 
 for (let i = 0; i < logo.length; i++) {
   logo[i].addEventListener("click", function () {
     location.href = "index.html";
   });
 }
+
+let headerOnScroll = document.querySelector(".header-onscroll-down");
+console.log(headerOnScroll);
+window.addEventListener("scroll", function () {
+  console.log("scrolling...");
+  headerOnScroll.classList.add("visibility");
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    headerOnScroll.classList.remove("display_none");
+  } else {
+    headerOnScroll.classList.add("display_none");
+  }
+});
